6f6d356eba66401398fb5c682ecfb6dc635b498d
kai.jiang <kai.jiang@easystack.cn>
2021-07-09 15:29:30 +0800

    Adjust code struction

--- cmd/podexec/podexec.go
+++ cmd/podexec/podexec.go
@@ -13,8 +13,7 @@ import (
 )
 
 var (
-	addr = flag.String("addr", "0.0.0.0:90", "http service address")
-	//cmd  = []string{"/bin/sh"}
+	addr         = flag.String("addr", "0.0.0.0:90", "http service address")
 	exec_command = []string{"/bin/sh", "-c", "TERM=xterm-256color; export TERM; [ -x /bin/bash ] && ([ -x /usr/bin/script ] && /usr/bin/script -q -c \"/bin/bash\" /dev/null || exec /bin/bash) || exec /bin/sh"}
 )
 
